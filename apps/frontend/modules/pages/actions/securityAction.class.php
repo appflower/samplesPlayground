309f3e572252cdbf6f2244998b4bd673c52bdf3f
Tamas Geschitz <getamas@gmail.com>
2012-01-25 13:21:20 +0100

    Enabled afGuard security panel

---
+++ apps/frontend/modules/pages/actions/securityAction.class.php
@@ -0,0 +1,16 @@
+ <?php
+/**
+ * Generated page action class
+ */
+class securityAction extends sfAction
+{
+    /**
+     * Execute actions
+     *
+     * @param sfWebRequest $request 
+     */
+    public function execute($request)
+    {
+    }
+    
+}
