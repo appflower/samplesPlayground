f055bc0ba3cdb57e5d619e6e33fcbb856820f522
Radu Topala <radu@appflower.com>
2012-01-26 22:13:19 +0200

    #1381 - finished master-detail example, example is inside dashboard /#/#master-detail

--- apps/frontend/modules/product/actions/editmdAction.class.php
+++ apps/frontend/modules/product/actions/editmdAction.class.php
@@ -2,21 +2,41 @@
 /**
  * @author Radu Topala <radu@appflower.com>
  */
-class editmdAction extends simpleWidgetEditAction
+class editmdAction extends sfAction
 {
 	
 	public function execute($request)
     {
-        if ($request->isMethod('post')) {
-            if ($this->processPostData()) {
-                $result = array(
-                    'success' => true,
-                    'message' => "Saved with success!",
-                    'redirect' => $this->widgetUri . '?id=' . $this->object->getId()
-                );
-                
-                return $result;
+        if (!$request->isMethod('post')) {
+                    
+            $product = productPeer::retrieveByPk($this->getRequestParameter('id'));
+            if($product!=null)
+            {
+                $result['data']['edit[7][id]']=$product->getId();
+                $result['data']['edit[7][name]'] = $product->getName();
+                $result['data']['edit[7][price]'] = $product->getPrice();
+                $result['data']['edit[7][quantity]'] = $product->getQuantity();
+                    
+                $result['success']=true;            
             }
+            else {
+                $result['success']=false;
+            }
+            
+            return $this->renderText(json_encode($result));
+        }
+        else {
+            $formData = $this->getRequestParameter('edit');
+            $formData = $formData['7'];
+            $product = ProductPeer::retrieveByPK($formData['id']);
+            $product->setName($formData['name']);
+  		    $product->setPrice($formData['price']);
+  		    $product->setQuantity($formData['quantity']);
+  		    $product->save();
+  		    
+  		    $result['success']=true;
+  		    
+  		    return $this->renderText(json_encode($result));
         }
     }
 }
