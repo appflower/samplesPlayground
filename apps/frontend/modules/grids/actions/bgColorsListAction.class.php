19319f9a967e4415abed9d3f0f9ce494659c339d
Radu Topala <radu@appflower.com>
2012-01-30 14:53:08 +0200

    moved list samples to grids module

---
+++ apps/frontend/modules/grids/actions/bgColorsListAction.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Generated action list class
+ */
+class bgColorsListAction extends sfAction
+{
+    /**
+     * Execute actions
+     *
+     * @param sfWebRequest $request 
+     */
+    public function execute($request)
+    {
+    }
+    
+}
---
+++ apps/frontend/modules/grids/actions/rowActionsListAction.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Generated action list class
+ */
+class rowActionsListAction extends sfAction
+{
+    /**
+     * Execute actions
+     *
+     * @param sfWebRequest $request 
+     */
+    public function execute($request)
+    {
+    }
+    
+}
